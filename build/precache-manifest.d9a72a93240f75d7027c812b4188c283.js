22616e232ab7e57906ef43afe6bf1cb16bb17518
ivan <ivan_sibir@mail.ru>
2019-03-10 15:44:53 +0700

    added build in repo

---
+++ build/precache-manifest.d9a72a93240f75d7027c812b4188c283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "651bf01a071ccbee42f4",
+    "url": "/static/js/main.651bf01a.chunk.js"
+  },
+  {
+    "revision": "9b6c3c11ba4ec9509e75",
+    "url": "/static/js/2.9b6c3c11.chunk.js"
+  },
+  {
+    "revision": "651bf01a071ccbee42f4",
+    "url": "/static/css/main.52d1c4cb.chunk.css"
+  },
+  {
+    "revision": "9b6c3c11ba4ec9509e75",
+    "url": "/static/css/2.1231cf45.chunk.css"
+  },
+  {
+    "revision": "4657c2dd4b622b5190b3eb9d20475e1d",
+    "url": "/index.html"
+  }
+];
