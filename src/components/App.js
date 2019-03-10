0bd3a0b2ed0e3f6502d3454e39c76735abc92eb5
ivan <ivan_sibir@mail.ru>
2019-03-10 15:00:49 +0700

    added root components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
-import filmsFetchSaga from './sagas'
-import reducer from './reducers'
+import fetchFilmsSaga from '../sagas/index'
+import { combineReducers } from 'redux'
+import films from '../reducers/films'
+import pages from '../reducers/pages'
+import search from '../reducers/search'
 import { createStore, applyMiddleware } from 'redux'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import '../styles/App.css'
+import SearchFilmAppContainer from "../containers/SearchFilmAppContainer";
+import FilmCard from "./FilmCard";
+
+const reducer = combineReducers({
+    films,
+    pages,
+    search
+});
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     reducer,
-    applyMiddleware(sagaMiddleware)
+    applyMiddleware(sagaMiddleware, logger)
 );
 
-sagaMiddleware.run(filmsFetchSaga);
+sagaMiddleware.run(fetchFilmsSaga);
 
-const WrongRoute = 
+/**
+ * 404 component for wrong query
+ */
+const WrongRoute = props => <h1>Wrong url, dude!</h1>;
+
+const FilmContainer = ({match: {params: {id: filmId}}}) => <FilmCard filmId={filmId}/>;
 
 class App extends Component {
   render() {
@@ -24,7 +41,8 @@ class App extends Component {
         <Provider store={store}>
           <Router>
             <Switch>
-              <Route path="/" exact component={} />
+              <Route path="/" exact component={SearchFilmAppContainer} />
+              <Route path="/films/:id" component={FilmContainer} />
               <Route component={WrongRoute} />
             </Switch>
           </Router>
